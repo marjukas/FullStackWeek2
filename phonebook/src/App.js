6a8f8264d55038244309649d40d854c93fd347af
Marju Uusen <marju@progmatic.ee>
2020-03-28 19:28:10 +0200

    functionality implemented except the search

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import Person from './components/Person'
 
-function App() {
+const App = () => {
+  const [ persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '213456' }
+  ]);
+  const [ newName, setNewName ] = useState('');
+  const [newNumber, setNewNumber] = useState('')
+
+    const addPerson = (event) => {
+      event.preventDefault();
+      if(!persons.some(person => person.name === newName)) {
+          setPersons(persons.concat({name: newName, number: newNumber}));
+      } else {
+          alert(newName + ' is already added to phonebook')
+      }
+        setNewName('')
+        setNewNumber('')
+    };
+
+    const onNameChange = (event) => {
+      setNewName(event.target.value);
+    };
+
+    const onNumberChange = event => {
+        setNewNumber(event.target.value);
+    };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+      <div>
+        <h2>Phonebook</h2>
+        <form onSubmit={addPerson}>
+          <div>
+            name: <input
+              value={newName}
+              onChange={onNameChange}
+          />
+          </div>
+            <div>
+              number: <input
+              value={newNumber}
+              onChange={onNumberChange}
+          />
+          </div>
+          <div>
+            <button type="submit">add</button>
+          </div>
+        </form>
+        <h2>Numbers</h2>
+        <ul>
+          {persons.map((person) =>
+              <Person key={person.name} person={person} />
+          )}
+        </ul>
+      </div>
+  )
+};
 
-export default App;
+export default App
